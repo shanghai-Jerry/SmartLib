3f59b409df0b8688eb54cf404f1771b0dac4e80e
shanghai-Jerry <cjyou20110920sspu@163.com>
2015-08-13 17:12:07 +0800

    WebQRC

---
+++ WebQRC/WebQRC/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Text;
+using ThoughtWorks;
+using ThoughtWorks.QRCode;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+using System.Drawing;
+
+using System.IO;
+
+namespace WebQRC
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string temp2=Request.QueryString["url"];
+            this.TextBox2.Text = Request.QueryString["id"];
+            this.TextBox1.Text = temp2 + "&rid=" + this.TextBox2.Text;
+            create_two(this.TextBox1.Text, this.TextBox2.Text);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            create_two(this.TextBox1.Text,this.TextBox2.Text);
+        }
+        private void create_two(string nr,string name)
+        {
+            Bitmap bt;
+            string enCodeString = nr;
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            bt=qrCodeEncoder.Encode(enCodeString,Encoding.UTF8);//注意编码
+            bool dx = File.Exists("~/image/" + name + ".jpg");
+            if (dx)
+            {
+                File.Delete("~/image/" + name + ".jpg");
+                bt.Save(Server.MapPath("~/image/" + name + ".jpg"));
+                this.Image1.ImageUrl = "~/image/" + name + ".jpg";
+
+            }
+            else
+            {
+                bt.Save(Server.MapPath("~/image/" + name + ".jpg"));
+                this.Image1.ImageUrl = "~/image/" + name + ".jpg";
+            }
+            
+        }
+    }
+}
---
+++ WebQRC/WebQRC/obj/Release/Package/PackageTmp/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebQRC.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server" Width="247px"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Generate" />
+    
+    </div>
+        <asp:Image ID="Image1" runat="server" Height="140px" Width="144px" />
+    </form>
+</body>
+</html>
